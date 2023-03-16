0f9d73cba5ca5fc5c31257c8639036cf8480aa6e
Laridn <larissa.nasci@hotmail.com>
2023-03-15 23:20:18 -0300

    Exercício pratico módulo 12 - Criando um teste com todo o fluxo de compra de 4 itens do site loja.ebac

---
+++ cypress/e2e/e2e.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
+  beforeEach(() => {
+    cy.visit('produtos');
+  });
+
+  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+    cy.addProdutos('Abominable Hoodie', 'XS', 'Blue', 1);
+    cy.get('.tbay-woocommerce-breadcrumb > :nth-child(5) > a').click();
+    cy.addProdutos('Hollister Backyard Sweatshirt', 'S', 'Red', 1);
+    cy.get('.tbay-woocommerce-breadcrumb > :nth-child(4) > a').click();
+    cy.addProdutos('Beaumont Summit Kit', 'M', 'Orange', 1);
+    cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2) > a').click();
+    cy.addProdutos('Ajax Full-Zip Sweatshirt', 'L', 'Red', 1);
+    cy.get('.woocommerce-message > .button').click();
+    cy.get('.checkout-button').click();
+    cy.addDados(
+      'Teste',
+      'Cypress',
+      'Ebac',
+      'Brasil',
+      'Rua da Paz',
+      '15',
+      'São Paulo',
+      'São Paulo',
+      '68928-166',
+      '11999999999',
+      'teste@teste87.com'
+    );
+    cy.get('#order_comments').type('OBS: Teste automatizado Cypress');
+    cy.get('#payment_method_cod').click();
+    cy.get('#terms').click();
+    cy.get('#place_order').click();
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,10 +24,51 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-
 Cypress.Commands.add('login', (usuario, senha) => {
-    cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
-    cy.get('.woocommerce-form > .button').click()
+  cy.get('#username').type(usuario);
+  cy.get('#password').type(senha, { log: false });
+  cy.get('.woocommerce-form > .button').click();
 });
 
+Cypress.Commands.add('addProdutos', (produto, tamanho, cor, quantidade) => {
+  cy.get('[class="product-block grid"]').contains(produto).click();
+  cy.get('.button-variable-item-' + tamanho).click();
+  cy.get('.button-variable-item-' + cor).click();
+  cy.get('.input-text').clear().type(quantidade);
+  cy.get('.single_add_to_cart_button').click();
+});
+
+Cypress.Commands.add(
+  'addDados',
+  (
+    nome,
+    sobrenome,
+    empresa,
+    pais,
+    endereco,
+    numero,
+    cidade,
+    estado,
+    cep,
+    telefone,
+    email
+  ) => {
+    cy.get('#billing_first_name').type(nome);
+    cy.get('#billing_last_name').type(sobrenome);
+    cy.get('#billing_company').type(empresa);
+    cy.get('#select2-billing_country-container')
+      .click()
+      .type(pais)
+      .get('[aria-selected="true"]')
+      .click();
+    cy.get('#billing_address_1').type(endereco);
+    cy.get('#billing_address_2').type(numero);
+    cy.get('#billing_city').type(cidade);
+    cy.get('#select2-billing_state-container')
+      .click()
+      .type(estado + '{enter}');
+    cy.get('#billing_postcode').type(cep);
+    cy.get('#billing_phone').type(telefone);
+    cy.get('#billing_email').type(email);
+  }
+);
