cbd27c6473962c4be3562563cd787851fd982286
Carlos Tavares <cmmt08@gmail.com>
2024-01-19 22:23:51 -0300

    exercicio ebac

--- cypress/integration/exercicio-e2e.spec.js
+++ cypress/integration/exercicio-e2e.spec.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+var faker = require('faker');
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -10,12 +11,23 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('/produtos/')
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(nomeFaker)
+
+        cy.pedidos('Aero Daily Fitness Tee', 'M', 'Brown', 1)
+        cy.visit('/produtos')
+        cy.pedidos('Arcadio Gym Short', '34', 'Blue', 3)
+        cy.visit('/produtos')
+        cy.pedidos('Argus All-Weather Tank', 'S', 'Gray', 2)
+        cy.visit('/produtos')
+        cy.pedidos('Aether Gym Pant', '34', 'Brown', 5)
+        cy.get('.woocommerce-message > .button').click()
+        cy.get('.checkout-button').click()
+        cy.finalizarcompra(nomeFaker, sobrenomeFaker, 'Rua X', 'Rio de Janeiro', 'Rio de Janeiro', '22776-000', '2199999999', emailFaker)
     });
-
-
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,7 +27,30 @@
 
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha, { log: false })
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('pedidos', (modelo, tamanho, cor, quantidade) => {
+    cy.get('[class="products products-grid"]').contains(modelo).click()
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-' + cor).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+});
+
+Cypress.Commands.add('finalizarcompra', (primeironome, ultimonome, endereco, cidade, estado, cep, numero, email) => {
+    cy.get('#billing_first_name').type(primeironome)
+    cy.get('#billing_last_name').type(ultimonome)
+    cy.get('#select2-billing_country-container').click().type('Brasil{enter}')
+    cy.get('#billing_address_1').type(endereco)
+    cy.get('#billing_city').type(cidade)
+    cy.get('#select2-billing_state-container').click().type(estado + '{enter}')
+    cy.get('#billing_postcode').type(cep)
+    cy.get('#billing_phone').type(numero)
+    cy.get('#billing_email').type(email)
+    cy.get('#createaccount').click()
+    cy.get('#account_password').type('Teste@teste123!')
+    cy.get('#terms').click()
+    cy.get('#place_order').click()
+});
