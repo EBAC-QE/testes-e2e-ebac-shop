b06e989e3d62dea91675cc18d645d9484411e9d8
Carlos Tavares <cmmt08@gmail.com>
2024-01-23 13:44:21 -0300

    alteracoes nos comandos customizados e adicionei um resultado esperado

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,13 +31,40 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
-Cypress.Commands.add('pedidos', (modelo, tamanho, cor, quantidade) => {
-    cy.get('[class="products products-grid"]').contains(modelo).click()
-    cy.get('.button-variable-item-' + tamanho).click()
-    cy.get('.button-variable-item-' + cor).click()
-    cy.get('.input-text').clear().type(quantidade)
+Cypress.Commands.add('pedidos1', (modelo1, tamanho1, cor1, quantidade1, modelo2, tamanho2, cor2, quantidade2, modelo3, tamanho3, cor3, quantidade3, modelo4, tamanho4, cor4, quantidade4 ) => {
+    cy.get('[class="products products-grid"]').contains(modelo1).click()
+    cy.get('.button-variable-item-' + tamanho1).click()
+    cy.get('.button-variable-item-' + cor1).click()
+    cy.get('.input-text').clear().type(quantidade1)
     cy.get('.single_add_to_cart_button').click()
 });
+Cypress.Commands.add('pedidos2', (modelo2, tamanho2, cor2, quantidade2) => {
+    cy.visit('/produtos')
+    cy.get('[class="products products-grid"]').contains(modelo2).click()
+    cy.get('.button-variable-item-' + tamanho2).click()
+    cy.get('.button-variable-item-' + cor2).click()
+    cy.get('.input-text').clear().type(quantidade2)
+    cy.get('.single_add_to_cart_button').click()
+});
+
+Cypress.Commands.add('pedidos3', (modelo3, tamanho3, cor3, quantidade3) => {
+    cy.visit('/produtos')
+    cy.get('[class="products products-grid"]').contains(modelo3).click()
+    cy.get('.button-variable-item-' + tamanho3).click()
+    cy.get('.button-variable-item-' + cor3).click()
+    cy.get('.input-text').clear().type(quantidade3)
+    cy.get('.single_add_to_cart_button').click()   
+})
+
+Cypress.Commands.add('pedidos4', (modelo4, tamanho4, cor4, quantidade4) => {
+    cy.visit('/produtos')
+    cy.get('[class="products products-grid"]').contains(modelo4).click()
+    cy.get('.button-variable-item-' + tamanho4).click()
+    cy.get('.button-variable-item-' + cor4).click()
+    cy.get('.input-text').clear().type(quantidade4)
+    cy.get('.single_add_to_cart_button').click()
+})
+
 
 Cypress.Commands.add('finalizarcompra', (primeironome, ultimonome, endereco, cidade, estado, cep, numero, email) => {
     cy.get('#billing_first_name').type(primeironome)
